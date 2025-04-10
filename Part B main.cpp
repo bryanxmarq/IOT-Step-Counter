e9d1dced521a391bb6dc257ebdcc1f22066bf561
bryanxmarq <98122010+bryanxmarq@users.noreply.github.com>
2025-04-10 09:54:48 -0700

    Update Part B main.cpp

--- Part B main.cpp
+++ Part B main.cpp
@@ -8,22 +8,22 @@
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
-// IMU object
 LSM6DSO myIMU;  
 
 BLECharacteristic *pCharacteristic;
 
-int stepCount = 0;
-float threshold = 1.5; // Calibrate this
-bool stepDetected = false;
+int stepCount = 0; //total number of steps
+float threshold = 1.5; //rms threshold
+bool stepDetected = false; //to prevent multiple detections
 
 
 void setup() {
   Serial.begin(115200);
 
-  Wire.begin();  // Initialize I2C with defined pins
+  Wire.begin();  // initialize I2C
   delay(10);
 
+  //check and initialize the IMU
   Serial.println("Trying to initialize IMU...");
   if (myIMU.begin()) {
     Serial.println("IMU initialized!");
@@ -33,6 +33,7 @@ void setup() {
 
   myIMU.initialize(BASIC_SETTINGS);
 
+  //create BLE service
   BLEDevice::init("StepCounter");
   BLEServer *pServer = BLEDevice::createServer();
   BLEService *pService = pServer->createService(SERVICE_UUID);
@@ -43,7 +44,7 @@ void setup() {
     BLECharacteristic::PROPERTY_NOTIFY
   );
 
-  pCharacteristic->setValue("Zero Steps"); // Initialize with 0
+  pCharacteristic->setValue("Zero Steps"); // Initialize with Zero Steps
   pService->start();
 
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
@@ -57,14 +58,18 @@ void setup() {
 void loop() {
   static unsigned long lastRead = 0;
 
+  //read acceleration every 20ms
   if (millis() - lastRead >= 20) {
     lastRead = millis();
 
+    //gest x and y axis
     float accX = myIMU.readFloatAccelX();
     float accY = myIMU.readFloatAccelY();
 
+    //calculate root mean square of x and y axis
     float rms = sqrt(accX * accX + accY * accY);
 
+    //check if step is detected
     if (rms > threshold && !stepDetected) {
       stepCount++;
       stepDetected = true;
@@ -72,13 +77,15 @@ void loop() {
       Serial.print("Step detected! Total: ");
       Serial.println(stepCount);
 
+      //convert count to string and send it over BLE
       pCharacteristic->setValue(String(stepCount).c_str());
       pCharacteristic->notify();
     }
 
+    //reset if step detected is below threshold
     if (rms < threshold - 0.2) {
       stepDetected = false;
     }
   }
-  delay(10); // Small delay to prevent excessive CPU usage
+  delay(10); 
 }
